c32403dc9b6c272f29f2c6e5033fa7cf26283498
Steven Bondaruk <bondarus@tcd.ie>
2022-04-01 00:52:20 +0100

    Updated TST to work with strings and queue update

--- src/TST.java
+++ src/TST.java
@@ -4,13 +4,12 @@ import java.util.*;
 public class TST {
 
     private int n; //size
-    private Node root; //root of TST
+    private Node<String> root; //root of TST
 
-    private static class Node
-    {
-        private String val;
-        private char c;
-        private Node left, mid, right;
+    private static class Node<String> {
+        private char c;                         // character
+        private Node<String> left, mid, right;  // left, middle, and right subtries
+        private String val;                     // value associated with string
     }
 
     public boolean contains(String key) {
@@ -20,51 +19,67 @@ public class TST {
         return get(key) != null;
     }
 
-    public String get(String key)
-    {
-        Node x = get(root, key, 0);
+    public String get(String key) {
+        if (key == null) {
+            throw new IllegalArgumentException("calls get() with null argument");
+        }
+        if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
+        Node<String> x = get(root, key, 0);
         if (x == null) return null;
         return x.val;
     }
 
-    private Node get(Node x, String key, int d)
-    {
+    private Node<String> get(Node<String> x, String key, int d) {
         if (x == null) return null;
+        if (key.length() == 0) throw new IllegalArgumentException("key must have length >= 1");
         char c = key.charAt(d);
-        if (c < x.c) return get(x.left, key, d);
-        else if (c > x.c) return get(x.right, key, d);
-        else if (d < key.length() - 1) return get(x.mid, key, d+1);
-        else return x;
+        if      (c < x.c)              return get(x.left,  key, d);
+        else if (c > x.c)              return get(x.right, key, d);
+        else if (d < key.length() - 1) return get(x.mid,   key, d+1);
+        else                           return x;
     }
 
-    public void put(String key, String val)
-    { root = put(root, key, val, 0); }
+    public void put(String key, String val) {
+        if (key == null) {
+            throw new IllegalArgumentException("calls put() with null key");
+        }
+        if (!contains(key)) n++;
+        else if(val == null) n--;       // delete existing key
+        root = put(root, key, val, 0);
+    }
 
-    private Node put(Node x, String key, String val, int d)
-    {
+    private Node<String> put(Node<String> x, String key, String val, int d) {
         char c = key.charAt(d);
-        if (x == null) { x = new Node(); x.c = c; }
-        if (c < x.c) x.left = put(x.left, key, val, d);
-        else if (c > x.c) x.right = put(x.right, key, val, d);
-        else if (d < key.length() - 1) x.mid = put(x.mid, key, val, d+1);
-        else x.val = val;
+        if (x == null) {
+            x = new Node<String>();
+            x.c = c;
+        }
+        if      (c < x.c)               x.left  = put(x.left,  key, val, d);
+        else if (c > x.c)               x.right = put(x.right, key, val, d);
+        else if (d < key.length() - 1)  x.mid   = put(x.mid,   key, val, d+1);
+        else                            x.val   = val;
         return x;
     }
 
-    public Iterable<String> keysWithPrefix(String prefix)
-    {
-        Queue<String> queue = new Queue<String>();
-        Node x = get(root, prefix, 0);
-        collect(x, prefix, queue);
+    public Iterable<String> keysWithPrefix(String prefix) {
+        if (prefix == null) {
+            throw new IllegalArgumentException("calls keysWithPrefix() with null argument");
+        }
+        Queue<String> queue = new LinkedList<String>();
+        Node<String> x = get(root, prefix, 0);
+        if (x == null) return queue;
+        if (x.val != null) queue.add(prefix);
+        collect(x.mid, new StringBuilder(prefix), queue);
         return queue;
     }
 
-    private void collect(Node x, String prefix, Queue<String> q)
-    {
+    private void collect(Node<String> x, StringBuilder prefix, Queue<String> queue) {
         if (x == null) return;
-        if (x.val != null) q.enqueue(prefix);
-        for (char c = 0; c < R; c++)
-            collect(x.next[c], prefix + c, q);
+        collect(x.left,  prefix, queue);
+        if (x.val != null) queue.add(prefix.toString() + x.c);
+        collect(x.mid,   prefix.append(x.c), queue);
+        prefix.deleteCharAt(prefix.length() - 1);
+        collect(x.right, prefix, queue);
     }
 
     public TST(String fileInput)
