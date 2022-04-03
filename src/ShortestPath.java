52278e0e526ef48eec9b51f953d4128ac579514c
Steven Bondaruk <bondarus@tcd.ie>
2022-04-03 16:44:40 +0100

    Updated ShortestPath(Dijkstra)

--- src/ShortestPath.java
+++ src/ShortestPath.java
@@ -12,7 +12,24 @@ public class ShortestPath {
         File busStopTimes = new File(busStopTimesFile);
         Scanner sc3 = new Scanner(busStopTimes);
 
+        while(sc1.hasNextLine())
+        {
+            String stop = sc1.nextLine();
+            String array[] = stop.split(",");
 
+        }
+        while(sc2.hasNextLine())
+        {
+            String edge = sc2.nextLine();
+            String array[] = edge.split(",");
+
+        }
+        while(sc3.hasNextLine())
+        {
+            String stop_time = sc3.nextLine();
+            String array[] = stop_time.split(",");
+
+        }
 
     }
 
